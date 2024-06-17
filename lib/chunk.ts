ee9c3285852857bd762e4e5d86b8776e9595130f
LoveFanta <2686414856@qq.com>
2024-06-17 23:36:48 +0800

    fix:chunk.ts

--- lib/chunk.ts
+++ lib/chunk.ts
@@ -1,34 +1,36 @@
-const handleInputFile = () => {
-    let inputFile = document.querySelector('#fileInput') as HTMLElement
-    let files = inputFile.files[0]
-    const chunks = creatChunk(files, 10 * 1024)
-    console.log(chunks);
-    hash(chunks)
-  }
+import SparkMD5 from 'spark-md5'
 
-  const hash = (chunks) => {
-    const spark = new SparkMD5()
-    function _read(i) {
-      if(i >= chunks.length){
-        console.log(spark.end())
-        return
-      }
-      const blob = chunks[i]
-      const reader = new FileReader()
-      reader.onload = (e) => {
-        const bytes = e.target.result
-        spark.append(bytes)
-        _read(i + 1)
-      }
-      reader.readAsArrayBuffer(blob)
-    }
-    _read(0)
-  }
+const handleInputFile = (inputName:string) => {
+  let inputFile = document.querySelector(inputName) as HTMLInputElement
+  if(!inputFile) return 
+  let files = inputFile.files as FileList
+  const chunks = creatChunk(files[0], 10 * 1024)
+  hash(chunks)
+}
 
-  const creatChunk = (file, chunkSize) => {
-    const result = []
-    for (let i = 0; i < file.size; i += chunkSize) {
-      result.push(file.slice(i, i + chunkSize, file.type))
+const hash = (chunks:Blob[]) => {
+  const spark = new SparkMD5.ArrayBuffer()
+  function _read(i:number) {
+    if (i >= chunks.length) {
+      console.log(spark.end())
+      return
     }
-    return result
+    const blob = chunks[i]
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      const bytes = (e.target && e.target.result) as ArrayBuffer
+      spark.append(bytes)
+      _read(i + 1)
+    }
+    reader.readAsArrayBuffer(blob)
   }
+  _read(0)
+}
+
+const creatChunk = (file: File, chunkSize: number) => {
+  const result = []
+  for (let i = 0; i < file.size; i += chunkSize) {
+    result.push(file.slice(i, i + chunkSize, file.type))
+  }
+  return result
+}
